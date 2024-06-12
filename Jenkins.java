d9f5980fc96fa999b385ffa67c8080c701ea1d8c
Nikhil-Kumar-C <162301715+Nikhil-Kumar-C@users.noreply.github.com>
2024-06-12 12:21:35 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -2,5 +2,7 @@
 public class Jenkins {
     public static void main(String[] args) {
         System.out.println("My name is Nikhil Kumar C");
+        System.out.println("My SRN is PES1UG22CS821");
+        System.out.println("Today is Wednesday");
     }
 }
